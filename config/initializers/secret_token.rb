2cbc907d197824f2c4005c5618d5e5992da50d65
Mike Kolganov <mkolganov@thumbtack.net>
2012-12-26 19:35:01 +0700

    Initial commit: rails new dpla_portal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DplaPortal::Application.config.secret_token = 'a8e5c82658f63f540c68b5a5e0b7c915222718baab5f80e66ba16e7b353fbb9d1163724df3a793d0c9000b3b3c24bb19f1fb54ee93e515c08fe68fb44c8d4bcb'
