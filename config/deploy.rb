ce7efec0f7fc485dec54b5d88431b66e339ccab8
Mike Kolganov <mike.kolganov@gmail.com>
2013-01-17 20:35:15 +0700

    Capistrano config updated

--- config/deploy.rb
+++
@@ -1,7 +0,0 @@
-require 'capistrano/ext/multistage'
-
-set :scm, :git
-set :repository,  "git://github.com/dpla/frontend.git"
-set :stages, %w(development staging)
-set :keep_releases, 5
-after "deploy:restart", "deploy:cleanup"
