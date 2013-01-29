58b3b432bab3b28e573ee6fad1bffa869e98ec74
Dmitry Nikitin <dmitry.nop@gmail.com>
2013-01-29 19:47:18 +0700

    Template for item page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
   def search
     @search = Item::Search.build params
+    session[:last_query] = request.url
   end
 
   def show
     @item = Item.find(params[:id]).first
     raise ActionController::RoutingError.new('Not Found') unless @item
+    logger.debug @item.inspect
   end
 end
