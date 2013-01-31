a5a1934805a518124df270def03462f796f2e6e8
Mike Kolganov <mike.kolganov@gmail.com>
2013-01-31 18:40:30 +0700

    Debug info removed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,5 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id]).first
     raise ActionController::RoutingError.new('Not Found') unless @item
-    logger.debug @item.inspect
   end
 end
