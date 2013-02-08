80175b0545a9543b7f4064166ae6156f2d98aea6
Mike Kolganov <mike.kolganov@gmail.com>
2013-02-08 22:06:19 +0700

    Implementation in progress

--- lib/dpla/item.rb
+++ lib/dpla/item.rb
@@ -12,5 +12,31 @@ module DPLA
     def self.by_conditions(options = {})
       # DPLA::Result
     end
+
+    private
+
+    # URI encode and prepare query string by internal conditions
+    # - when String - just URI encode
+    # - when Array - encode elements and join by slash(/)
+    # - when Hash - build query string regarding special rules
+    def self.prepare_conditions(conditions = {})
+      encode_uri = ->(v) {URI.encode(v.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}
+      prepare_value = ->(*args) {
+        value = args.pop
+        key = args.join('.')
+      }
+
+      if [Array, String].include? conditions.class
+        return Array(conditions).map { |v| encode_uri.call v }.join '/'
+      else
+        [].tap do |result|
+          conditions.each do |key, value|
+            if [Numeric, String].include? value.class
+              result << "#{key}=#{encode_uri.call(value)}"
+            end
+          end
+        end.join '&'
+      end
+    end
   end
 end
--- spec/lib/dpla/item_spec.rb
+++ spec/lib/dpla/item_spec.rb
@@ -1,31 +1,67 @@
 require 'spec_helper'
 
 describe DPLA::Item do
-  context 'preparing conditions' do
-    subject { DPLA::Item.new }
+  subject { DPLA::Item }
 
-    it 'should returns simple keys transparent' do
-      conditions = { q: 'civil war', foo: 'bar', count: 1, facets: 'format' }
-      expected   = 'q=civil%20war&foo=bar&count=1&facets=format'
-      subject.prepare_conditions(conditions).should eql(expected)
+  context 'preparing_conditions' do
+    context 'string' do
+      it 'should uri encode string' do
+        prepare_conditions('new york').should eql('new%20york')
+      end
     end
 
-    it 'should flatten nested hash' do
-      conditions = { spatial: { city: 'New York City', state: 'NY'} }
-      expected   = "spatial.city=New%20York%20City&spatial.state=NY"
-      subject.prepare_conditions(conditions).should eql(expected)
+    context 'array' do
+      it 'should uri encode elements' do
+        prepare_conditions(['new york']).should eql('new%20york')
+      end
+
+      it 'should join elements by slash' do
+        prepare_conditions(['some string', 'new york'])
+          .should eql('some%20string/new%20york')
+      end
     end
 
-    it 'should transform facets array to comma-separated string' do
-      conditions = { facets: ['subject.name', 'format'] }
-      expected   = 'facets=subject.name,format'
-      subject.prepare_conditions(conditions).should eql(expected)
+    context 'hash' do
+      it 'should uri encode elements' do
+        prepare_conditions(q: 'new york')
+          .should eql('q=new%20york')
+      end
+
+      it 'should transform hash to uri params' do
+        prepare_conditions(q: 'new york', subject: 'City Library')
+          .should eql('q=new%20york&subject=City%20Library')
+      end
+
+      it 'should flatten nested hashes to dot-separated keys' do
+        prepare_conditions(created: {start: '1980-12-10', end: '2000-12-12'})
+          .should eql('created.start=1980-12-10&created.end=2000-12-12')
+      end
+
+      it 'should join :facets and :fields arrays by comma' do
+        prepare_conditions(facets: ['spatial', 'language'])
+          .should eql('facets=spatial,language')
+        prepare_conditions(fields: ['id', 'subject', 'description'])
+          .should eql('fields=id,subject,description')
+      end
+
+      it 'should join array values by +AND+' do
+        prepare_conditions(language: ['English', 'Russian'], type: ['text', 'image'])
+          .should eql('language=English+AND+Russian&type=text+AND+image')
+      end
+
+      it 'should wrap :subject values by doublequotes' do
+        prepare_conditions(subject: ['Ships', 'Shipyards', "New York"])
+          .should eql('subject="Ships"+AND+"Shipyards"+AND+"New%20York"')
+      end
+
+      it 'should not wrap :sort_by, :sort_order, :page, :page_size by doublequotes' do
+        prepare_conditions(sort_by: 'subject.name', sort_order: :asc, page: 2, page_size: 20)
+          .should eql('sort_by=subject.name&sort_order=asc&page=2&page_size=20')
+      end
     end
 
-    it 'should transform other arrays to "+AND+"-separated string' do
-      conditions = { subject: ["First subject", "Second"] }
-      expected   = 'subject=First%20subject+AND+Second'
-      subject.prepare_conditions(conditions).should eql(expected)
+    def prepare_conditions(*args)
+      subject.send :prepare_conditions, *args
     end
   end
 end
