b76d32c716e3b9b482a7999bfad365dea242ffa8
Dmitry Nikitin <dmitry.nop@gmail.com>
2013-02-25 22:54:08 +0700

    Login and signup in a process

--- app/assets/javascripts/login.js.coffee
+++ app/assets/javascripts/login.js.coffee
@@ -1,4 +1,9 @@
-# jQuery ->
+jQuery ->
+  $('#loginBox form#sign_in').on 'ajax:success', (e, data) ->
+    if data.success
+      alert 'success'
+    else
+      alert 'not'
 #   $('#loginBox').on 'click', 'input', (event) ->
 #     if $(this).attr('value') == 'Log In'
 #       alert "Login"
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < Devise::SessionsController
+  def create
+    resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+    sign_in(resource_name, resource)
+    render json: {
+      success: true #, content: render_to_string(:layout => false, :partial => 'sessions/manager')
+    }
+  end
+
+  def failure
+    render json: {
+      success: false, errors: ["Login failed."]
+    }
+  end
 end
