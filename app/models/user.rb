0bce9b1df776715989f2b8aed233dc3c52768984
Dmitry Nikitin <dmitry.nop@gmail.com>
2013-02-26 18:57:16 +0700

    Password field not required

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
-  validates :name, :email, :password, presence: true
+  validates :name, presence: true
 
   def active_for_authentication?
     true
