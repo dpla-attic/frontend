296ffb290779f8f92718f9658555f88683b184db
Dmitry Nikitin <dmitry.nop@gmail.com>
2013-02-27 14:12:35 +0700

    Removed old templates and unused code

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,11 +1,2 @@
 class PasswordsController < Devise::PasswordsController
-  def create
-    respond_to do |format|
-      format.html { super }
-      format.json do
-        resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
-        return sign_in_and_redirect(resource_name, resource)
-      end
-    end
-  end
 end
