7e25be7a43c353b76053163c2635f71bdce2b5e6
Dmitry N <dmitry.nop@gmail.com>
2013-03-03 22:00:19 +0700

    Leaflet integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,3 +31,4 @@
 //= require spikes.js.coffee
 //= require common.js.coffee
 //= require map.js
+//= require settings.js
---
+++ app/assets/javascripts/settings.js
@@ -0,0 +1,80 @@
+var settings = {
+
+
+
+        numberOfBestResults : 2,
+	
+	points : [ {
+		number : 0,
+		lat    : 51.5241, 
+		lon    : -0.1734,
+		image  : "img/map-popup.jpg",
+                text   : "Text 0",
+                title  : "Hi, George",
+                href   : "Title of Result Lorem Ipsum sit Dolor Amet.",
+                author : "By Jonathan Doe",
+                score  : 0
+	},  {
+		number : 1,
+		lat : 51.4892, 
+		lon : -0.1596,
+		image  : "img/map-popup.jpg",
+                text   : "Text -1",
+                title  : "Hi, George  -1",
+                href   : "Title of Result Lorem Ipsum sit Dolor Amet.",
+                author : "By Jonathan Doe",
+                score  : -1
+	},{
+		number : 2,
+		lat : 51.4886, 
+		lon : -0.0988,
+		image  : "img/map-popup.jpg",
+                text   : "Text 100",
+                title  : "Hi, George 100",
+                href   : "Title of Result Lorem Ipsum sit Dolor Amet.",
+                author : "By Jonathan Doe",
+                score  : 100
+	},{
+		number : 3,
+		lat : 51.5141, 
+		lon : -0.0734,
+		image  : "img/map-popup.jpg",
+                text   : "Text 2",
+                title  : "Hi, George 2",
+                href   : "Title of Result Lorem Ipsum sit Dolor Amet.",
+                author : "By Jonathan Doe",
+                score  : 2
+	},  {
+		number : 4,
+		lat : 51.4992, 
+		lon : -0.1396,
+		image  : "img/map-popup.jpg",
+                text   : "Text 1",
+                title  : "Hi, George 1",
+                href   : "Title of Result Lorem Ipsum sit Dolor Amet.",
+                author : "By Jonathan Doe",
+                score  : 1
+	},{
+		number : 5,
+		lat : 51.5341, 
+		lon : -0.1434,
+		image  : "img/map-popup.jpg",
+                text   : "Text 3",
+                title  : "Hi, George 3",
+                href   : "Title of Result Lorem Ipsum sit Dolor Amet.",
+                author : "By Jonathan Doe",
+                score  : 3
+	},  {
+		number : 6,
+		lat : 51.4862, 
+		lon : -0.1396,
+		image  : "img/map-popup.jpg",
+                text   : "Text 2",
+                title  : "Hi, George 2",
+                href   : "Title of Result Lorem Ipsum sit Dolor Amet.",
+                author : "By Jonathan Doe",
+                score  : 2
+	}
+    ]
+
+};
