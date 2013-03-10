e0aba90052194b4a630be767ccbc653010004bd7
Dmitry N <dmitry.nop@gmail.com>
2013-03-11 02:43:21 +0700

    Back to search results returns on right page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,6 @@ class SearchController < ApplicationController
   def list
     @search = Search.new *permitted_params.search
     @items = @search.result permitted_params.args
-    session[:last_query] = request.url
   end
 
   private
