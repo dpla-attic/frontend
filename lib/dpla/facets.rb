58d19c0a6e37853e04dc84a733ea1299621fe5ae
Dmitry Nikitin <dmitry.nop@gmail.com>
2013-03-10 20:20:02 +0700

    Facets updated for new schema

--- lib/dpla/facets.rb
+++ lib/dpla/facets.rb
@@ -4,26 +4,23 @@ module DPLA
 
     def initialize(facets)
       facets.each do |key, value|
-        if 'subject.name' == key and value['terms'].is_a? Array
+        case key
+        when 'aggregatedCHO.subject.name'
           @subject = {}.tap do |subject|
             value['terms'].each { |term| subject[term['term']] = term['count'] }
-          end
-        elsif 'language.name' == key and value['terms'].is_a? Array
+          end if value['terms'].is_a? Array
+        when 'aggregatedCHO.language.name'
           @language = {}.tap do |subject|
             value['terms'].each { |term| subject[term['term']] = term['count'] }
-          end
-        elsif 'type' == key and value['terms'].is_a? Array
+          end if value['terms'].is_a? Array
+        when 'aggregatedCHO.type'
           @type = {}.tap do |subject|
             value['terms'].each { |term| subject[term['term']] = term['count'] }
-          end
-        elsif 'created.start.year' == key and value['entries'].is_a? Array
+          end if value['terms'].is_a? Array
+        when 'aggregatedCHO.date.begin.year'
           @year = {}.tap do |subject|
             value['entries'].each { |entry| subject[entry['time']] = entry['count'] }
-          end
-        elsif 'created.start.decade' == key and value['entries'].is_a? Array
-          @decade = {}.tap do |subject|
-            value['entries'].each { |entry| subject[entry['time']] = entry['count'] }
-          end
+          end if value['entries'].is_a? Array
         end
       end if facets.is_a? Hash
     end
