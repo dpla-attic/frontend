83ec630eb3db2ffe84dcf64309fc850f61881a20
Mike Kolganov <mike.kolganov@gmail.com>
2013-03-15 16:48:52 +0700

    Links to Exhibitions CMS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,16 @@ module ApplicationHelper
   def resource_name
     :user
   end
- 
+
   def resource
     @resource ||= User.new
   end
- 
+
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def exhibitions_path
+    Settings.exhibitions.site
   end
 end
