fc2a8efae44dcb3a9b14be05d56494de2f6a9801
Mike Kolganov <mike.kolganov@gmail.com>
2013-03-18 17:44:21 +0700

    Flash messages added to layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,2 @@
 class SessionsController < Devise::SessionsController
-
-  def sign_in_and_redirect(resource_or_scope, resource=nil)
-    scope = Devise::Mapping.find_scope!(resource_or_scope)
-    resource ||= resource_or_scope
-    sign_in(scope, resource) unless warden.user(scope) == resource
-    return render :json => {:success => true, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource)}
-  end
-  
 end
