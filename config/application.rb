850807b47bfb42483483d1eb28ec8cd8b3c851f5
Mike Kolganov <mike.kolganov@gmail.com>
2013-03-26 15:18:08 +0700

    Assets version bump

--- config/application.rb
+++ config/application.rb
@@ -68,7 +68,7 @@ module DplaPortal
     config.assets.precompile += %w( .svg .eot .woff .ttf )
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
     config.paths['app/views'] << "app/views/devise"
   end
 end
