7ac3109ffdadf5b63a1a4720422b3f3ad9d2e6ed
Mike Kolganov <mike.kolganov@gmail.com>
2013-03-28 20:18:01 +0700

    Saved searches: getting lists for item changed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,13 +3,9 @@ class ItemsController < ApplicationController
     @item = DPLA::Items.by_ids(params[:id]).first
     raise ActionController::RoutingError.new('Not Found') unless @item
 
-    item_lists = current_user.saved_items
-      .includes(:saved_lists)
-      .where(item_id: params[:id])
-      .group('saved_lists.id')
-      .map { |i| i.saved_lists }
-      .flatten
-      .compact
+    item_lists = current_user.saved_lists
+      .includes(:saved_items)
+      .where('saved_items.item_id' => params[:id])
     @lists = current_user.saved_lists - item_lists
   end
 end
