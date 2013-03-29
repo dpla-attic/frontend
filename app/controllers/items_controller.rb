d99ab268a32e669b6f95e8ed428344dca22e4634
Vitaly Ulaev <vulaev@thumbtack.net>
2013-03-29 12:51:55 +0700

    fixed bug with showing items when user is not signed in

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,11 @@ class ItemsController < ApplicationController
     @item = DPLA::Items.by_ids(params[:id]).first
     raise ActionController::RoutingError.new('Not Found') unless @item
 
-    item_lists = current_user.saved_lists
-      .includes(:saved_items)
-      .where('saved_items.item_id' => params[:id])
-    @lists = current_user.saved_lists - item_lists
+    if user_signed_in?
+       item_lists = current_user.saved_lists
+         .includes(:saved_items)
+         .where('saved_items.item_id' => params[:id])
+       @lists = current_user.saved_lists - item_lists
+    end
   end
 end
