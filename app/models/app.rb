dec6a62d0b4ed461605d6165716d5dff01c5ce28
Mike Kolganov <mike.kolganov@gmail.com>
2013-04-14 20:41:44 +0700

    Task #24412: make possible to set applicaiton created time.

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,6 +1,7 @@
 class App < ActiveRecord::Base
   attr_accessible :title, :description, :teaser_description,
-    :author, :is_promo, :home_page_url, :develop_app_url, :icon, :screenshot
+    :author, :is_promo, :home_page_url, :develop_app_url, :icon, :screenshot,
+    :created_at
   mount_uploader :icon, AppIconUploader
   mount_uploader :screenshot, AppScreenshotUploader
 
