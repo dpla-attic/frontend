16a1e8f33b469f53fb910c36fae69bb615df23c3
Mike Kolganov <mike.kolganov@gmail.com>
2013-05-06 13:36:54 +0700

    DPLA Branding gem switching improved

--- Gemfile
+++ Gemfile
@@ -45,4 +45,6 @@ group :test, :development do
   gem 'shoulda-matchers'
 end
 
-# gem 'dpla_frontend_assets', git: 'git@github.com:dpla/frontend-assets.git'
+group :dpla_branding do
+  gem 'dpla_frontend_assets', git: 'git@github.com:dpla/frontend-assets.git'
+end
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:dpla_branding) if Bundler.settings.without.exclude? :dpla_branding
 end
 
 module DplaPortal
