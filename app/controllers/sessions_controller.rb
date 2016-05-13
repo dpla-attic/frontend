8c768b54b33ce92d0dbead2def280a1c7bfec20e
Mark Breedlove <mb@dp.la>
2016-05-13 13:02:36 -0400

    Avoid setting Set-Cookie header unnecessarily
    Remove ApplicationController's behavior to unset the "no cache" cookie
    with every request for a user who is not logged-in. Instead, unset the
    cookie when a logged-in user logs out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,11 @@ class SessionsController < Devise::SessionsController
 
   def set_cache_flag!
     flag = Settings.session.logged_in_flag.to_sym
-    cookies[flag] = { value: 1, httponly: true } if not cookies[flag]
+    if current_user
+      cookies[flag] = { value: 1, httponly: true } if not cookies[flag]
+    else
+      cookies.delete flag
+    end
   end
 
   def new
